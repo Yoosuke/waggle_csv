02ac3e2fd6764c37681941333abd943da96e1788
Yoosuke <yoosukenakao@thewaggle.co.jp>
2022-04-24 18:18:25 +0900

    version down

--- waggle_csv/mix.exs
+++ waggle_csv/mix.exs
@@ -4,7 +4,7 @@ defmodule WaggleCsv.MixProject do
   def project do
     [
       app: :waggle_csv,
-      version: "0.1.0",
+      version: "0.0.1",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps()
